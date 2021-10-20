3855c8ce9c2c002d4dfdb10bbaa76a0bcc365a9b
muhridddinmusoqulov <you@example.com>
2021-10-20 16:06:47 +0500

    dars joylandii

---
+++ 1-DARS.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+ 
+ int main() {
+ 	
+ 		int mening_raqamim;
+ 	int sening_raqaming;
+ 	
+ 	mening_raqamim = 14;
+ 	sening_raqaming = 13;
+ 	
+ 	int yigindi;
+ 	
+ 	yigindi = mening_raqamim + sening_raqaming;
+ 	
+ 	printf("%d", yigindi );
+ 	
+ 	return 0;
+ }
+ 
